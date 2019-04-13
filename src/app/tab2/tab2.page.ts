0e7319c1dc220178792c9f12cedcd1e0b902787e
Sleeyax <yourd3veloper@gmail.com>
2019-04-13 19:36:37 +0200

    Applied light and dark themes color palette

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {NavController} from "@ionic/angular";
 import {HttpClient} from "@angular/common/http";
 import {SettingsService} from "../services/settings.service";
-import {FormControl, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, Validators} from "@angular/forms";
 import {BarcodeScanner} from "@ionic-native/barcode-scanner/ngx";
 
 @Component({
@@ -18,21 +18,23 @@ export class Tab2Page implements OnInit{
     ngOnInit(): void {
         // Validate user input of key input field
         this.keyFormControl = new FormControl('',
-            Validators.compose([Validators.required, Validators.pattern('[a-zA-Z-0-9]+')]),
-            Validators.composeAsync([(): any => {
-                return new Promise(async resolve => {
-                    const resolved = await this.verifyKey();
-                    resolve(resolved ? resolved : {required: "Enter a valid key"});
-                });
-            }])
+            Validators.compose([Validators.pattern('[a-zA-Z-0-9]+')]),
         );
     }
     /**
      * Navigate to file info page
      */
     public async navFileInfo() {
-        if (this.keyFormControl.valid) {
+        if (this.keyFormControl.value == '') {
+            this.keyFormControl.setErrors({msg: 'Please enter a key'});
+            return;
+        }
+
+        const keyValid = await this.verifyKey(this.keyFormControl.value);
+        if (keyValid) {
             this.nav.navigateForward(`fileinfo/${this.keyFormControl.value}`);
+        }else{
+            this.keyFormControl.setErrors({msg: 'Invalid key'});
         }
     }
 
@@ -48,9 +50,9 @@ export class Tab2Page implements OnInit{
     /**
      * Check whether or not the key exists
      */
-    public async verifyKey() {
+    public async verifyKey(key: string) {
         return new Promise((resolve, reject) => {
-            this.http.get(`${this.settings.server}/file/verify/${this.keyFormControl.value}`).subscribe((res) => {
+            this.http.get(`${this.settings.server}/file/verify/${key}`).subscribe((res) => {
                 resolve(res["found"]);
             }, (err) => {console.log(err); reject(false);});
         });
